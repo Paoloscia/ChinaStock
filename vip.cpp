8201ec225794e44fa3bfa2fc85a1125f3b5415c8
Paoloscia <56681786+Paoloscia@users.noreply.github.com>
2020-04-05 14:36:30 +0200

    base iniziale vip //incompleta

--- vip.cpp
+++ vip.cpp
@@ -1,6 +1,6 @@
 #include "vip.h"
 
-Vip::Vip()
+Vip::Vip(++++, Cliente cli, data d,string n,bool cn) :clientepiscina(cli), datascadenza(d), nomeistruttorepiscina (n), corsonuoto(cn)
 {
-
+    clientipiscina++;
 }
