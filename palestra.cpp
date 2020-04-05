23b6335af39de7eee409be1ccb78be64780f3035
eliarech <eliasherek@gmail.com>
2020-04-05 13:26:21 +0200

    aggiunta bozza palestra

--- palestra.cpp
+++ palestra.cpp
@@ -1,6 +1,49 @@
 #include "palestra.h"
 
-Palestra::Palestra()
+palestra::palestra(char n, char c, unsigned int g, unsigned int m, unsigned int a,string cf, char lN, char r, char v, unsigned int num,string nt,string em,bool s,bool sc,string ni,unsigned int sg,unsigned int sm,unsigned int sa):Cliente(n,c,g,m,a,cf,lN,r,v,num,nt,em,s),scheda(sc),nomeistruttore(ni),sgiorno(sg),smese(sm),sanno(sa){}
+
+palestra *palestra::clone() const
+{
+    return new palestra(*this);
+}
+/*
+palestra::palestra(const palestra &p)
 {
 
 }
+
+bool palestra::operator==(const Cliente &c) const
+{
+
+}
+
+bool palestra::operator!=(const Cliente &c) const
+{
+
+}
+*/
+bool palestra::isscheda() const
+{
+    return scheda;
+}
+
+string palestra::getnomeistruttore() const
+{
+    return nomeistruttore;
+}
+
+unsigned int palestra::getsgiorno() const
+{
+    return sgiorno;
+}
+
+unsigned int palestra::getsmese() const
+{
+    return smese;
+}
+
+unsigned int palestra::getsanno() const
+{
+    return sanno;
+}
+
--- palestra.h
+++ palestra.h
@@ -1,11 +1,33 @@
 #ifndef PALESTRA_H
 #define PALESTRA_H
+#include "cliente.h"
 
-
-class Palestra
+class palestra : public Cliente
 {
+private:
+    bool scheda; //se fa la scheda oppure no;
+    string nomeistruttore; //nome istruttore;
+    unsigned int sgiorno; //scadenza giorno
+    unsigned int smese;  // scadenza mese
+    unsigned int sanno;  // scadenza anno
 public:
-    Palestra();
+    palestra(char,char,unsigned int,unsigned int,unsigned int,string,char, char,char,unsigned int,string,string,bool,bool,string,unsigned int,unsigned int,unsigned int);
+    //palestra(const palestra& p);
+
+    virtual palestra* clone() const;
+    //~palestra() override;
+    //virtual bool operator==(const Cliente& c) const; //override?
+    //virtual bool operator!=(const Cliente& c) const; //override?
+
+
+    bool isscheda() const;
+    string getnomeistruttore() const;
+     /////////////DA DISCUTERE
+
+    unsigned int getsgiorno() const;
+    unsigned int getsmese() const;
+    unsigned int getsanno() const;
+
 };
 
 #endif // PALESTRA_H
