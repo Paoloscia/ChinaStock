eda0a15f12a112adf255088e22eed1e2df2b84f1
Unknown <xa-bit@hotmail.it>
2020-04-05 12:30:53 +0200

    aggiunta modifica bozze piscina .h e .cpp
    aggiunte piscina.h e piscina.cpp bozze, bisognerà sistemare classe data che è stata inclusa e non è presente nel progetto

--- piscina.cpp
+++ piscina.cpp
@@ -1,6 +1,9 @@
 #include "piscina.h"
 
-Piscina::Piscina()
-{
-
-}
+int Piscina::clientipiscina = 0; //contatore statico per numero clienti piscina
+Piscina::Piscina(Cliente cli, data d,string n,bool cn) :clientepiscina(cli), datascadenza(d), nomeistruttorepiscina (n), corsonuoto(cn){clientipiscina++;}
+Piscina::~Piscina(){clientipiscina--;}
+data Piscina::getscadenzaabbonamento() const {return datascadenza;}
+string Piscina::getnomeistruttorepiscina() const {return nomeistruttorepiscina;}
+bool Piscina::getcorsonuoto() const {return corsonuoto;}
+int Piscina::getclientipiscina(){return clientipiscina;}
