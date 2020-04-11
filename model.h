a89d9265019d7cec19de3cafeddaa6dfcc6f7e0e
Unknown <xa-bit@hotmail.it>
2020-04-11 11:31:59 +0200

    aggiunti metodi e dati su classe model

--- model.h
+++ model.h
@@ -3,8 +3,7 @@
 
 #include "container.h"
 #include "cliente.h"
-
-
+#include "QString"
 
 class model
 {
@@ -17,6 +16,16 @@ public:
     void cancellaOggetto(cliente *, bool = true);
 
     void salva();
+
+    //partire da clear, reset filter e costruttore
+
+    //clear serve a distruggere tutto il container datitotali, è il nostro distruggi
+
+private:
+    QString path; //serve per prendere i dati dal percorso del file
+    Container<cliente *> *datiTotali; //Vedere se effettivamente è giusto
+    Container<cliente *> *datiFiltrati; //Vedere se effettivamente è giusto
+    bool modificato; //teoricamente serve per non risalvare se non è stato modificato nulla
 };
 
 #endif // MODEL_H
