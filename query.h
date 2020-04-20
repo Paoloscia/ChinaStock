b0c80d0a3fca22fefd9b4f4e3d8f9ac35178c4fd
eliarech <44705161+eliarech@users.noreply.github.com>
2020-04-20 18:48:30 +0200

    aggiunto query

--- query.h
+++ query.h
@@ -5,8 +5,9 @@ using std::string;
 
 class query
 {
-/*public:
-    query(string,string, int, int, int,string,string, string,string,unsigned int,string,string,bool, bool, bool, bool);//ultimi 3 booleani sono abbonamento piscina, palestra o vip
+public:
+    query(string nome,string cognome, int giorno, int mese, int anno,string codicefiscale,string luogodN, string residenza,string via,unsigned int numvia,string numerotel,string mail,bool studente, bool palestra, bool piscina);//ultimi 3 booleani sono abbonamento piscina, palestra o vip
+    //ho tolto l'ultimo bool perchè è VIP
     string getnome() const;
     string getcognome() const;
     string getcodfiscale() const;
@@ -16,7 +17,28 @@ class query
     unsigned int getnum() const;
     string getnumerotel() const;
     string getmail() const;
-    bool getstudent() const;*/
+    bool getstudent() const;
+    bool getpiscina() const;
+    bool getpalestra() const;
+    bool getvip() const;
+private:
+    string nome;
+    string cognome;
+    unsigned int giorno;
+    unsigned int mese;
+    unsigned int anno;
+    string codicefiscale;
+
+    string luogo;
+    string residenza;
+    string via;
+    unsigned int numero;
+    string numerotelefono;
+    string mail;
+    bool studente;
+    bool piscina;
+    bool palestra;
+    bool vip;
 };
 
 
